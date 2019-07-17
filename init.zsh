d7e691a46d7eff5f51824e1ca933bd6ae3f99f20
Thomas Hanson <46658215+diresqrl@users.noreply.github.com>
2019-07-16 22:12:35 -0700

    updated init

--- init.zsh
+++ init.zsh
@@ -1,2 +1,17 @@
 0=${(%):-%N}
 source ${0:A:h}/zsh-history-substring-search.zsh
+
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=green,fg=white,bold'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
+# Emacs mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
